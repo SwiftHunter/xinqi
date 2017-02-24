5d6294b05eb5b1ec0f7fe18572bdf4191855b752
DESKTOP-S9VO937\liujun <572266316@qq.com>
2017-02-25 00:54:05 +0800

    信旗酒店用品ssm框架搭建

---
+++ xinqi-manager/xinqi-manager-pojo/src/main/java/com/liujun/entity/Item.java
@@ -0,0 +1,23 @@
+package com.liujun.entity;
+
+public class Item {
+    private Integer id;
+
+    private String itemName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName == null ? null : itemName.trim();
+    }
+}
